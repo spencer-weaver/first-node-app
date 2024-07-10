e6ce63f2b036ce8662d883c5732b007cb44ea94c
spencer-weaver <sweavertheslayr@outlook.com>
2024-07-10 18:49:00 -0400

    Added yaml and return html on get

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { readFile, readFileSync } = require('fs');
 
 const app = express();
 
 app.get('/', (request, response) => {
 
-    
+    readFile('./home.html', 'utf8', (err, html) => {
+
+        if (err) {
+            response.status(500).send('sorry, out of order')
+        }
+
+        response.send(html);
+
+    })
 
 });
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'))
